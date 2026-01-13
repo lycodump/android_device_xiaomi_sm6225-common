fbb989d682509c9cb800f813f78f13dc2317c0f3
Liekoo <veyeka1@gmail.com>
2026-01-13 01:52:15 +0000

    sm6225-common: fixup offending entries

--- common.mk
+++ common.mk
@@ -121,7 +121,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     libcamera2ndk_vendor \
-    libdng_sdk.vendor \
     libgui_vendor \
     libstdc++_vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
