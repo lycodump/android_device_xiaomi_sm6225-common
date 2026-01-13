f987d5858c29653ae94a07327ab26c32b56c819c
Michael Bestas <mikeioannina@gmail.com>
2026-01-13 21:26:07 +0800

    sm6225-common: Patch some blobs to depend on libtinyxml2-v34.so

--- extract-files.py
+++ extract-files.py
@@ -84,6 +84,9 @@ blob_fixups: blob_fixups_user_type = {
     'system_ext/lib64/vendor.qti.hardware.qccsyshal@1.2-halimpl.so': blob_fixup()
         .replace_needed('libprotobuf-cpp-full.so', 'libprotobuf-cpp-full-21.7.so'),
 
+    'vendor/lib64/libdpps.so': blob_fixup()
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
+
 }
 
 module = ExtractUtilsModule(
